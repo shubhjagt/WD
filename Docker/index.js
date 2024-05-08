1f1bdf50086bf38699c4c2f0241213f3a8b300c1
NirajShinde03 <shindeniraj084@gmail.com>
2024-05-08 18:39:11 +0530

    All

---
+++ Docker/index.js
@@ -0,0 +1,10 @@
+console.log("Name: Om Golesar")
+console.log("Roll No: 17")
+console.log("Class: TE")
+console.log("Batch: A")
+console.log("Subject: LP-II WAD")
+
+//docker --version
+//node index.js
+//docker build -t doc .
+//docker images
