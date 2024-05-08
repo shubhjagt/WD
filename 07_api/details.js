6f6723747b3f2491552fab39e6c23dff139a7a6b
NirajShinde03 <shindeniraj084@gmail.com>
2024-05-09 00:28:08 +0530

    .

---
+++ 07_api/details.js
@@ -0,0 +1,26 @@
+// index.js
+// mongodb.js
+
+// --------------------------------------------------------------------------------------------------------
+
+// create database & collection:-
+// database:- student
+// collection:- profile
+
+// add data in the format:-
+
+//             {
+//                 "_id": {
+//                 "$oid": "6621341182fcef8dec3f9d5a"
+//                 },
+//                 "name": "roshan",
+//                 "email": "roshan@gmail.com",
+//                 "location": "nashik"
+//             }
+
+// --------------------------------------------------------------------------------------------------------
+
+// npm init
+// npm install express
+// npm install mongodb
+// node index.js
